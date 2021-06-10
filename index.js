639fac179304a3afe19dece72a60ac69a4810f20
Swaraj <swarajrajpure@gmail.com>
2021-06-10 15:22:46 +0530

    shows the link to the user from which the image can be downloaded

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const submitBtn = document.getElementById("submit-btn");
 const fileUpload = document.getElementById("uploadedFile");
+const message = document.getElementById("message");
+const downloadBtn = document.getElementById("download-btn");
 
 const FILE_UPLOAD_API = 'http://localhost:4000/upload';
 
@@ -10,11 +12,15 @@ const uploadFile = async (formData) => {
   })
 
   const json = await response.json();
-  console.log(json);
+  message.innerText = 'Your tiny image is ready!'
+
+  downloadBtn.setAttribute("href", json.link);
+  downloadBtn.innerText = 'Save me..!';
 }
 
 submitBtn.addEventListener("click", async (e) => {
   e.preventDefault();
+  message.innerText = 'Please wait...';
   const file = fileUpload.files[0];
   const fileToSend = new FormData();
   fileToSend.append('image', file);
